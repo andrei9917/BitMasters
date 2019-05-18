192b6039de581c6d517fcdf021fdfd062e895b32
matdibu <matdibu@protonmail.com>
2019-05-19 01:09:24 +0300

    pls
    Signed-off-by: matdibu <matdibu@protonmail.com>

--- app/src/main/java/com/example/andrei/bitmasters/MainActivity.java
+++ app/src/main/java/com/example/andrei/bitmasters/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -45,8 +44,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, GenreSlidersActivity.class));
             }
         });
 
@@ -142,6 +142,7 @@ public class MainActivity extends AppCompatActivity
         // Create glide request manager
         RequestManager requestManager = Glide.with(this);
 // Create request builder and load image.
+
         RequestBuilder requestBuilder = requestManager.load(acct.getPhotoUrl());
 // Show image into target imageview.
         ImageView imageView = findViewById(R.id.imageView);
